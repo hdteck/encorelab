07a2d3ce5ab2a05653ba105493251ce8873bd26a
hdteck <45236600+hdteck@users.noreply.github.com>
2020-02-27 10:57:35 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const http = require('http');
+const http = require('https');
 const hostname = 'encorelab.scm.azurewebsites.net';
 const port = 3000;
 const server = http.createServer((req, res) => {
